4943031b12bfc4fa16eb13d71c0901a96470f43a
Celine <syuying07@163.com>
2022-09-01 08:13:00 +0800

    seal form

---
+++ src/base-ui/form/src/form.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="yu-form">
+    <el-form label-width="100px">
+      <el-row>
+        <template v-for="item in formItems" :key="item.label">
+          <el-col :span="8">
+            <el-form-item :label="item.label">
+              <template
+                v-if="item.type === 'input' || item.type === 'password'"
+              >
+                <el-input
+                  :placeholder="item.placeholder"
+                  show-password="item.type==='password'"
+                ></el-input>
+              </template>
+              <template v-else-if="item.type === 'select'">
+                <el-select :placeholder="item.placeholder" style="width: 100%">
+                  <el-option
+                    v-for="option in item.options"
+                    :value="option.value"
+                    :key="option.value"
+                    >{{ option.title }}
+                  </el-option>
+                </el-select>
+              </template>
+              <template v-else-if="item.type === 'datepicker'">
+                <el-date-picker v-bind="item.otherOptions"></el-date-picker>
+              </template>
+            </el-form-item>
+          </el-col>
+        </template>
+      </el-row>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { IFormItem } from '../types'
+
+export default defineComponent({
+  props: {
+    formItems: {
+      type: Array as PropType<IFormItem[]>,
+      default: () => []
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,17 +1,61 @@
 <template>
   <div class="user">
-    <div class="search"></div>
+    <div class="search">
+      <hy-form :formItems="formItems"></hy-form>
+    </div>
     <div class="content"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HyForm, { IFormItem } from '@/base-ui/form'
 
 export default defineComponent({
   name: 'user',
+  components: {
+    HyForm
+  },
   setup() {
-    return {}
+    const formItems: IFormItem[] = [
+      {
+        type: 'input',
+        label: 'id',
+        rules: [],
+        placeholder: '请输入id'
+      },
+      {
+        type: 'input',
+        label: '用户名',
+        rules: [],
+        placeholder: '请输入用户名'
+      },
+      {
+        type: 'password',
+        label: '密码',
+        placeholder: '请输入密码'
+      },
+      {
+        type: 'select',
+        label: '喜欢的运动',
+        placeholder: '请选择喜欢的运动',
+        options: [
+          { title: '篮球', value: 'basketball' },
+          { title: '足球', value: 'football' }
+        ]
+      },
+      {
+        type: 'datepicker',
+        label: '创建时间',
+        placeholder: '请输入创建时间',
+        otherOptions: {
+          startPlaceholder: '开始时间',
+          endPlaceholder: '结束时间',
+          type: 'daterange'
+        }
+      }
+    ]
+    return { formItems }
   }
 })
 </script>
