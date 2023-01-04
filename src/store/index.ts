f57d30c7c239fad3fee1e740b869d2fea400b07f
syy <syuying07@163.com>
2023-01-05 00:00:52 +0800

    role add modal

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,7 +13,8 @@ const store = createStore<IRootState>({
       name: 'yuying',
       age: 18,
       entireDepartment: [],
-      entireRole: []
+      entireRole: [],
+      entireMenu: []
     }
   },
   mutations: {
@@ -22,6 +23,9 @@ const store = createStore<IRootState>({
     },
     changeEntireRole(state, list) {
       state.entireRole = list
+    },
+    changeEntireMenu(state, list) {
+      state.entireMenu = list
     }
   },
   actions: {
@@ -37,9 +41,12 @@ const store = createStore<IRootState>({
         size: 1000
       })
       const { list: roleList } = rolePageList.data
+      const menuResult = await getPageListData('/menu/list', {})
+      const { list: menuList } = menuResult.data
       // 保存数据
       commit('changeEntireDepartment', departmentList)
       commit('changeEntireRole', roleList)
+      commit('changeEntireMenu', menuList)
     }
   },
   modules: { login, system }
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -4,29 +4,74 @@
     <page-content
       :contentTableConfig="contentTableConfig"
       pageName="role"
+      @newBtnClick="handleNewData"
+      @editBtnClick="handleEditData"
     ></page-content>
+    <page-modal
+      ref="pageModalRef"
+      :defaultInfo="defaultInfo"
+      :modalConfig="modalConfig"
+      pageName="role"
+    >
+      <div class="menu-tree">
+        <el-tree
+          :data="menus"
+          show-checkbox
+          node-key="id"
+          :props="{
+            children: 'children',
+            label: 'name'
+          }"
+        >
+        </el-tree>
+      </div>
+    </page-modal>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useStore } from '@/store'
 
 import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
+import PageModal from '@/components/page-modal'
 
 import { contentTableConfig } from './config/content.config'
 import { searchFormConfig } from './config/search.config'
+import { modalConfig } from './config/modal.config'
+
+import { usePageModal } from '@/hooks/use-page-modal'
 
 export default defineComponent({
   name: 'role',
   components: {
     PageContent,
-    PageSearch
+    PageSearch,
+    PageModal
   },
   setup() {
-    return { contentTableConfig, searchFormConfig }
+    const [pageModalRef, defaultInfo, handleNewData, handleEditData] =
+      usePageModal()
+
+    const store = useStore()
+    const menus = computed(() => store.state.entireMenu)
+    return {
+      contentTableConfig,
+      searchFormConfig,
+      modalConfig,
+      pageModalRef,
+      defaultInfo,
+      handleNewData,
+      handleEditData,
+      menus
+    }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.menu-tree {
+  margin-left: 20px;
+}
+</style>
